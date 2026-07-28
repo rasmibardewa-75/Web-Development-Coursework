5e6aea27b68e3cdb53feec9b6eba91be03129b54
rasmibardewa-75 <np03cy4a250050@heraldcollege.edu.np>
2026-07-28 13:45:51 +0545

    last  message

--- Web-Development-Coursework/Web-Development-Coursework-main/script.js
+++ Web-Development-Coursework/Web-Development-Coursework-main/script.js
@@ -1,22 +1,5 @@
-<script>
-function searchEvent() {
-    let searchInput = document.getElementById("searchInput").value.toLowerCase();
-
-    let eventTable = document.querySelector("table");
-    let rows = eventTable.getElementsByTagName("tr");
-
-    for (let i = 1; i < rows.length; i++) {
-        let eventName = rows[i].getElementsByTagName("td")[0];
-
-        if (eventName) {
-            let text = eventName.textContent.toLowerCase();
-
-            if (text.includes(searchInput)) {
-                rows[i].style.display = "";
-            } else {
-                rows[i].style.display = "none";
-            }
-        }
-    }
-}   
-</script>
+function searchEvent(){
+    let input= document.getElementById("searchInput").ariaValueMax.toLowerCase();
+    let table= document.getElementByTagName("table")[0];
+    let rows= table.getElementByTagName("tr");
+}
